96b923aba54a29f71ff81125bf662ec43d0591bc
Ana Garrido Diaz <anagarri@c1r2s2.42malaga.com>
2024-12-12 17:44:26 +0100

    add ft_printf.c and modify minishell.c

--- minishell.c
+++ minishell.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+int ft_putstr(char *string)
+{	
+	int len = 0;
+	while(*string != '\0')
+	{
+		len++;
+		string++;
+	}
+	return len;
+}
+
 int	main(void)
 {
-	printf("Hola Mundo!");
+	char *string = "Hola mundo";
+	printf("%i", ft_putstr(string));
 	return (0);
 }
